fa3403dd90ac47424e6d0870ce0bb2450aaaace1
Anton Sergushkin <antonbarada94@gmail.com>
2020-09-13 17:51:20 +0300

    feat: resolve 2 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  // checking and setting by task condition
+  if (typeof str !== "string") {
+    str += '';
+  }
   
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  if (!options.separator) {
+    options.separator = '+';
+  }
+
+  if (options.hasOwnProperty("addition")) {
+    if (typeof options.addition !== "string") {
+      options.addition += '';
+    }
+    if (!options.additionRepeatTimes) {
+      options.additionRepeatTimes = 1;
+    }
+    if (!options.additionSeparator) {
+      options.additionSeparator = "|";
+    }
+  } else {
+    options.addition = "";
+  }
+
+  //main algorithm
+  let result = [];
+  let additionValue = [];
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    let newStr = "";
+    newStr += options.addition;
+    additionValue.push(newStr);
+  }
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result.push(str + additionValue.join(options.additionSeparator))
+  }
+  return result.join(options.separator);
+};
